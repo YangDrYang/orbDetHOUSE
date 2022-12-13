16e0f43fd4e11b0308efcef673c6b9c1b6030481
James Appleton <james.appleton@optusnet.com.au>
2022-12-13 11:40:45 +1100

    Add eigen library, update compile script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 # Compile main library
 
-g++ -c *.cpp --std=c++11 -Wall -pedantic -g -O3
+echo "Compiling main library..."
+g++ -c *.cpp -I./eigen -I. -Wall -pedantic -g -O3 
 
 rm -f house.a
 
@@ -10,38 +11,42 @@ rm -f *.o
 
 # Compile projectile example
 
+echo "Compiling projectile..."
 cd projectile
 
-g++ -o projectile.exe projectile.cpp -I.. ../house.a --std=c++11 \
+g++ -o projectile.exe projectile.cpp -I.. -I./../eigen ../house.a \
              -O3 -g -Wall -pedantic
 
 cd ..
 
 # Compile rigid body example
+echo "Compiling rigid body..."
 
 cd rigid_body
 
-g++ -o rigid_body.exe rigid_body.cpp -I.. ../house.a --std=c++11 \
+g++ -o rigid_body.exe rigid_body.cpp -I.. -I./../eigen ../house.a \
              -O3 -g -Wall -pedantic
 
 cd ..
 
 # Compile coordinated turn example with Gaussian noise
+echo "Compiling CT with Gaussian Noise..."
 
 cd CT_Gauss
 
-g++ -o ct.exe CT.cpp ../house.a -I.. --std=c++11 -g -Wall -pedantic -O3
+g++ -o ct.exe CT.cpp ../house.a -I.. -I./../eigen  -g -Wall -pedantic -O3
 
-g++ -o st.exe statct.cpp -I.. --std=c++11 -g -Wall -pedantic -O3
+g++ -o st.exe statct.cpp -I.. -I./../eigen -g -Wall -pedantic -O3
 
 cd ..
 
 # Compile coordinated turn example with Pearson type IV noise
+echo "Compiling CT with Pearson Noise..."
 
 cd CT_Pearson
 
-g++ -o ct.exe CT.cpp ../house.a -I.. --std=c++11 -g -Wall -pedantic -O3
+g++ -o ct.exe CT.cpp ../house.a -I.. -I./../eigen -g -Wall -pedantic -O3
 
-g++ -o st.exe statct.cpp -I.. --std=c++11 -g -Wall -pedantic -O3
+g++ -o st.exe statct.cpp -I.. -I./../eigen -g -Wall -pedantic -O3
 
 cd ..
