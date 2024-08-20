215f4a409c822da2df58dde2ca70fc5810503485
Yang Yang <yiyinfeixiong@gmail.com>
2024-08-20 15:49:24 +1000

    new rkf78 integrator

--- filter/multiNormal.hpp
+++
@@ -1,9 +0,0 @@
-#include <boost/random.hpp>
-#include <boost/random/normal_distribution.hpp>
-#include <boost/numeric/ublas/vector.hpp>
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/io.hpp>
-
-namespace ublas = boost::numeric::ublas;
-
-ublas::vector<double> generateMultivariateNormal(boost::mt19937 &generator, ublas::matrix<double> covariance);
